c810f5fe9848ce22388a650a8d5d4956efc5bd99
Eigeen <dengyk2002@qq.com>
2025-09-24 16:53:55 +0800

    fix invalid utf8 test

--- tests/string.rs
+++ tests/string.rs
@@ -97,7 +97,7 @@ fn test_string_fmt_debug() -> Result<()> {
 
     // Invalid utf8
     let s = lua.create_string(b"hello\0world\r\n\t\xf0\x90\x80")?;
-    assert_eq!(format!("{s:?}"), r#"b"hello\0world\r\n\t\xf0\x90\x80""#);
+    assert_eq!(format!("{s:?}"), r#"b"hello\0world\r\n\t\xF0\x90\x80""#);
 
     Ok(())
 }
